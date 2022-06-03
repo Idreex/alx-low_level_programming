f45251a1aaff3fd6c7c3606e2adfd146d3f93824
Idreex <idreesademola07@gmail.com>
2022-06-02 18:34:09 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-/***main - Entry point * * Return: always 0*/ 
-int main(void)
-{
-	(prinf("with proper grammer, but the outcome is a piece of art,\n");
-	 return(0)	 }
+#include <stdio.h>/** * main - Entry point * * Return: Always 0 (Success) */int main(void){printf("with proper grammar, but the outcome is a piece of art,\n");return (0);}
+
+
