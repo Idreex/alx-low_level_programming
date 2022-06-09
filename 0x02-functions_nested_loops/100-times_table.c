8a252a236a54365bb8a3affa95c951d0f0d37198
Idreex <idreesademola07@gmail.com>
2022-06-08 19:18:42 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,13 +16,13 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
-				if (z > 99utchar)
+				if (z > 99)
 				{
 					_putchar(',');
 					_putchar(32);
 					_putchar(32);
 					_putchar(((z / 10) % 10) + '0');
-					_putchar((z % 10) + '0);
+					_putchar((z % 10) + '0');
 				}
 				else
 				{
@@ -40,6 +40,3 @@ void print_times_table(int n)
 		}
 	}
 }
-
-
-
