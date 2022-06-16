581093509ebf9e253e1ec66f3ef7552657cdb71e
Idreex <idreesademola07@gmail.com>
2022-06-16 14:01:20 -0700

    3-strcmp.c

---
+++ 3-strcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *_strcmp -  compares two strings
+ *@s1: A pointer to an char that will be updated
+ *@s2: A pointer to an char that will be updated
+ *Return: dest
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
