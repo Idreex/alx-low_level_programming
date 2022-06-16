45fb98dbdb3e229310887826d1c2e2026c03afc9
Idreex <idreesademola07@gmail.com>
2022-06-16 15:13:30 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,4 +1,15 @@
- upp_letters[] = {65, 69, 79, 84, 76};
+#include "main.h"
+/**
+ * leet - encodes a string into 1337
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *leet(char *s)
+{
+	int count = 0, i;
+	int low_letters[] = {97, 101, 111, 116, 108};
+	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
